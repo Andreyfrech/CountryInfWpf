67fb855e32fca86fd721654437f61617ae38c2ad
frech <andreyka977@list.ru>
2020-11-05 12:12:28 +0300

    Интерфейс для регулярных выражений

---
+++ CountryInfWpf/RegexCountry/IRegex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryInfWpf.RegexCountry
+{
+    //Интерфей для классов регулярных выражений
+    interface IRegex
+    {
+        List<string> regex(string Page);
+    }
+}
